5c2d26d54995cea277b12d5c9a932cc1a1e612e1
muzammi961 <muzammil2332005@gmail.com>
2025-08-21 16:51:51 +0530

    commit

--- src/commponent/display.jsx
+++ src/commponent/display.jsx
@@ -1,34 +1,46 @@
-import {addtodo,deletetodo} from "../createslice"
+import { addtodo, deletetodo, updatetodo } from "../createslice";
 import { useState } from "react";
-import {useDispatch,useSelector} from "react-redux"
-function Display(){
-let [task,useTask]=useState(" ")
-let {todo}=useSelector((state)=>state.data)
+import { useDispatch, useSelector } from "react-redux";
 
+function Display() {
+  let [task, setTask] = useState("");
+  let [editId, setEditId] = useState(null); 
+  let { todo } = useSelector((state) => state.data);
 
-let dispatch=useDispatch()
-let addfunc=()=>{
-    if(task.trim()){
-    dispatch(addtodo(task))
-    useTask('')
+  let dispatch = useDispatch();
+
+  let addfunc = () => {
+    if (task.trim()) {
+      if (editId) {
+        dispatch(updatetodo({ id: editId, newText: task }));
+        setEditId(null);
+      } else {
+        dispatch(addtodo(task));
+      }
+      setTask("");
     }
+  };
+
+  let handleEdit = (item) => {
+    setTask(item.text);   
+    setEditId(item.id);   
+};
+
+  return (
+    <>
+      <h1>To Do List</h1>
+
+      <input type="text" value={task} onChange={(e)=>setTask(e.target.value)}  placeholder="enter what do you want to do today  " />
+      <button onClick={addfunc}>Add </button>
+
+      {todo.map((d) => (
+        <div key={d.id}>
+          <h3>{d.text}</h3>
+          <button onClick={() => handleEdit(d)}>Edit</button>
+          <button onClick={() => dispatch(deletetodo(d.id))}>Delete</button>
+        </div>
+      ))}
+    </>
+  );
 }
-
-    return(<>
-
-    <h1>to do list</h1>
-    <input type="text" value={task} onChange={(e)=>useTask(e.target.value)} placeholder="enter the new task..." />
-    <button onClick={addfunc}>add</button>
-
-
-    {todo.map((d)=>(
-        <>
-        <h1>{d.text}</h1>
-        <button onClick={()=>dispatch(deletetodo(d.id))}>delete</button>
-        </>
-    ))}
-
-    </>)
-}
-
 export default Display;
--- src/createslice.jsx
+++ src/createslice.jsx
@@ -1,17 +1,24 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-let useslice=createSlice({
-    name:'todo',
-    initialState:{todo:[]},
-    reducers:{
-        addtodo:(state,action)=>{
-            state.todo.push({id:Date.now(),text:action.payload})
-        },
-        deletetodo:(state,action)=>{
-            state.todo=state.todo.filter((e)=>e.id !== action.payload)
-        }
+let useslice = createSlice({
+  name: 'todo',
+  initialState: { todo: [] },
+  reducers: {
+    addtodo: (state, action) => {
+      state.todo.push({ id: Date.now(), text: action.payload });
+    },
+    deletetodo: (state, action) => {
+      state.todo = state.todo.filter((e) => e.id !== action.payload);
+    },
+    updatetodo: (state, action) => {
+      const { id, newText } = action.payload; 
+      let todoItem = state.todo.find((e) => e.id === id);
+      if (todoItem) {
+        todoItem.text = newText;
+      }
     }
-})
+  }
+});
 
-export const {addtodo,deletetodo}= useslice.actions
-export default useslice.reducer
+export const { addtodo, deletetodo, updatetodo } = useslice.actions;
+export default useslice.reducer;
